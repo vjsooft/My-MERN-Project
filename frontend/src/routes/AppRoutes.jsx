67f617ea04ac08e14e7a07a3100ef3f2259bfbe8
vjsolution <vjsolution@gmail.com>
2026-08-24 09:07:03 +0530

    add 404 page as a wrong route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
-import {BrowserRouter, Routes,} from "react-router-dom"
+import {BrowserRouter, Routes,Route} from "react-router-dom"
 import PublicRoutes from "./PublicRoutes";
+import NotFound from "../pages/share-page/404-NotFound";
 const AppRoutes = () => {
   return (
     <BrowserRouter> 
         <Routes>
           {PublicRoutes()}
+        <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
   )
